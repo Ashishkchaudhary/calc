940ed27a4608dc1953d9a78d38907bfc1aaf48bb
Ashishkchaudhary <ashish.93kumark@gmail.com>
2019-09-13 13:02:47 +0530

    Updates

---
+++ precache-manifest.7b687aece54bdb19506867ce37b6628a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecb8350ac12dd94c088dbf748e97b3b",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "6b7d0ffb58701a6b9daa",
+    "url": "/calc/static/css/main.ac534661.chunk.css"
+  },
+  {
+    "revision": "ce3784147a5068090e10",
+    "url": "/calc/static/js/2.206fbce5.chunk.js"
+  },
+  {
+    "revision": "6b7d0ffb58701a6b9daa",
+    "url": "/calc/static/js/main.2c432557.chunk.js"
+  },
+  {
+    "revision": "e60d74e24ba15745e6e3",
+    "url": "/calc/static/js/runtime~main.2a1b0bf0.js"
+  }
+]);
